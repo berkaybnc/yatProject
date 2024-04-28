772a23c48b9c8aaa9a7dc39f17a6a37723f3704d
berkaybnc <97890442+berkaybnc@users.noreply.github.com>
2024-04-28 13:08:53 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+(function () {
+    var options = {
+        whatsapp: "+905067770046",
+        call_to_action: "Merhaba nasıl yardımcı olabiliriz?",
+        position: "right",
+    };
+    var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+})();
+
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
+
+// yorumlar
+
